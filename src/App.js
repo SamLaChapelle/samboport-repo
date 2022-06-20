844f284000508c73399a6c0c095f5bec519ab174
Sam LaChapelle <sambosauce@gmail.com>
2022-06-20 16:57:27 -0400

    header bio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './main css/App.css';
 import Header from "./components/header.js";
+import Code from "./components/code.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Code />
     </div>
   );
 }
