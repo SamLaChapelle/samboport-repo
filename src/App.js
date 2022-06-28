f93c9ae24e031f1a1a1dfcec2a0d9927161bce0b
Sam LaChapelle <sambosauce@gmail.com>
2022-06-28 00:11:37 -0400

    header complete / code component editing mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './main css/App.css';
+import "./main css/App.css";
 import Header from "./components/header.js";
 import Code from "./components/code.js";
 
 function App() {
   return (
     <div className="App">
+      <p id="htmlTextOne">&lt;html&gt;</p>
+      <p id="bodyTextOne">&lt;body&gt;</p>
+      <p id="headerTextOne">&lt;header&gt;</p>
       <Header />
+      <p id="headerTextTwo">&lt;/header&gt;</p>
+      <p id="projectsTextOne">&lt;projects&gt;</p>
       <Code />
+      <p id="projectsTextTwo">&lt;/projects&gt;</p>
+      <p id="bodyTextTwo">&lt;/body&gt;</p>
+      <p id="htmlTextTwo">&lt;/html&gt;</p>
     </div>
   );
 }
